b614b5c16cc413c9dd8136a5045e0d25a9fb3902
sender <tksendamasato@gmail.com>
2022-04-07 20:31:02 +0900

    Add files via upload

---
+++ 31-40/39.py
@@ -0,0 +1,19 @@
+# 問題:JOI 2011 予選 4 - 1 年生
+# URL:https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d
+
+n = int(input())
+suuretu = list(map(int, input().split()))
+ans = suuretu.pop()
+
+dp = [[0] * 21 for i in range(n - 1)]
+dp[0][suuretu[0]] = 1
+
+for i in range(n - 2):
+    for l in range(21):
+        if dp[i][l] != 0:
+            if l + suuretu[i + 1] <= 20:
+                dp[i + 1][l + suuretu[i + 1]] += dp[i][l]
+            if l - suuretu[i + 1] >= 0:
+                dp[i + 1][l - suuretu[i + 1]] += dp[i][l]
+
+print(dp[n - 2][ans])
